30b7cd9c08935f591bde2285bc8d4cfb9b476ad6
Jooa <code@jooa.xyz>
2020-12-03 17:30:48 +0100

    Modify selection differences for raw data

--- modules/Selection Differences/server.R
+++ modules/Selection Differences/server.R
@@ -11,24 +11,25 @@ function(input, output, session, opt) {
   dataframe <- reactive({
     if (isTruthy(rows())) {
       # We have a selection
-      indices <- 1:nrow(mat)
+      indices <- 1:nrow(rawmat)
+      m <- rawmat[,colSums(rawmat) > 20]
       select_bool <- indices %in% rows()
-      selected <- colSums(mat[select_bool,])
-      background <- colSums(mat[!select_bool,])
+      selected <- colSums(m[select_bool,])
+      background <- colSums(m[!select_bool,])
+      p.value <- apply(m, 2, function(r) wilcox.test(r[select_bool], r[!select_bool])$p.value)
+      total <- background+selected
       if (input$diff_mean) {
         selected <- selected / sum(select_bool)
         background <- background / sum(!select_bool)
       } 
-      if (input$diff_pseudo)
-        change <- (selected+1) / (background+1)
-      else
-        change <- selected / background
+      change <- selected / background
       d <- data.frame(
-        gene_symbol=colnames(mat),
+        gene_symbol=colnames(rawmat),
         foldchange=change,
         selected=selected,
         rest=background,
-        total=background+selected
+        p.value=p.value,
+        total=total
       )
       if (exists("cols"))
         d$id <- cols
@@ -57,11 +58,11 @@ function(input, output, session, opt) {
     dataframe()
   }, list(scrollX=T))
 
-  #output$diff_ma <- renderPlot({
-  #  ggplot(dataframe()) +
-  #    geom_point(aes(total, foldchange), alpha=0.2) +
-  #    scale_x_log10()
-  #})
+  output$diff_ma <- renderPlot({
+    ggplot(dataframe()) +
+      geom_point(aes(total, foldchange), alpha=0.2) +
+      scale_x_log10()
+  })
 
   output$diff_dl <- downloadHandler(
     filename = function() {
--- modules/Selection Differences/ui.R
+++ modules/Selection Differences/ui.R
@@ -1,18 +1,24 @@
-sidebarLayout(
-  sidebarPanel(
-    #selectizeInput("diff_vars", "Annotation Columns",
-    #               colnames(annotations), diff_default_anns,
-    #               multiple=T),
-    checkboxInput("diff_pseudo", "Pseudocounts", value=F),
-    checkboxInput("diff_mean", "Compute mean", value=F),
-    checkboxInput("diff_exclude", "Keep only finite foldchange", value=T),
-    checkboxInput("diff_log", "Log2 fold changes", value=T),
-    downloadButton("diff_dl", "Download CSV"),
-    hr(),
-    plotOutput("diff_ma")
-  ),
-  mainPanel(
-    dataTableOutput("diff_table"),
-    dataTableOutput("diff_go"),
+if (exists("rawmat")){
+  ui.this <- sidebarLayout(
+    sidebarPanel(
+      #selectizeInput("diff_vars", "Annotation Columns",
+      #               colnames(annotations), diff_default_anns,
+      #               multiple=T),
+      checkboxInput("diff_mean", "Compute mean", value=F),
+      checkboxInput("diff_exclude", "Keep only finite foldchange", value=T),
+      checkboxInput("diff_log", "Log2 fold changes", value=T),
+      downloadButton("diff_dl", "Download CSV"),
+      hr(),
+      helpText("Pseudocounts means adding a one to all counts.", "Computing the mean will compute the mean.", "Log2 fold changes will log-transform fold changes."),
+      hr(),
+      plotOutput("diff_ma")
+    ),
+    mainPanel(
+      dataTableOutput("diff_table"),
+      dataTableOutput("diff_go"),
+    )
   )
-)
+} else {
+  ui.this <- strong("No raw counts provided. Please contact whover deployed this app.")
+}
+ui.this
