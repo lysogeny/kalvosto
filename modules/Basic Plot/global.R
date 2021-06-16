a8ec0c1c4b33d533fbfef25a6e1db6729e7fba03
Jooa <code@jooa.xyz>
2021-06-16 14:20:53 +0200

    Filter out variables with too many levels from grouping
    Having too many levels for a grouping variable can freeze the server.
    This commit mitigates that issue by filtering out variables with more
    than 100 levels.

--- modules/Basic Plot/global.R
+++ modules/Basic Plot/global.R
@@ -4,3 +4,6 @@ basic_default_z <- meta$default_z
 basic_default_gene <- meta$default_gene
 basic_grouping_vars <- sapply(annotations, function(x) is.character(x) | is.factor(x) | is.logical(x))
 basic_grouping_vars <- names(basic_grouping_vars)[basic_grouping_vars]
+basic_grouping_vars_good <- unlist(lapply(basic_grouping_vars, function(name) length(unique(annotations[[name]])) > 100))
+str(basic_grouping_vars_good)
+basic_grouping_vars <- basic_grouping_vars[basic_grouping_vars_good]
